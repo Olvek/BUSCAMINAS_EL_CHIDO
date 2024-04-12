263043c5c76da3ef758a82607f82b39871c3f2c9
niko-ortega <dafne.ortega@uttn.mx>
2024-04-12 10:03:42 -0500

    PARCHE

--- www/public/js/game.js
+++ www/public/js/game.js
@@ -344,22 +344,22 @@ console.log(' Eg: `game = new Game(10, 10, 10, ["🌱", "💥", "🚩", "◻️"
 console.log(' Or: `game = new Game(16, 16, 30, ["✅", "💣", "🚧", "◻️"], true)`')
 
 
-document.addEventListener("DOMContentLoaded", function () {
-  var wrapper = document.querySelector('.wrapper');
-  var newGameButton = document.querySelector('.js-popup-new-game');
+// document.addEventListener("DOMContentLoaded", function () {
+//   var wrapper = document.querySelector('.wrapper');
+//   var newGameButton = document.querySelector('.js-popup-new-game');
 
-  var randomColor = 'rgb(' + Math.floor(Math.random() * 256) + ',' +
-    Math.floor(Math.random() * 256) + ',' +
-    Math.floor(Math.random() * 256) + ')';
-  wrapper.style.background = randomColor;
+//   var randomColor = 'rgb(' + Math.floor(Math.random() * 256) + ',' +
+//     Math.floor(Math.random() * 256) + ',' +
+//     Math.floor(Math.random() * 256) + ')';
+//   wrapper.style.background = randomColor;
 
-  newGameButton.addEventListener('click', function () {
-    var randomColor = 'rgb(' + Math.floor(Math.random() * 256) + ',' +
-      Math.floor(Math.random() * 256) + ',' +
-      Math.floor(Math.random() * 256) + ')';
-    wrapper.style.background = randomColor;
-  });
-});
+//   newGameButton.addEventListener('click', function () {
+//     var randomColor = 'rgb(' + Math.floor(Math.random() * 256) + ',' +
+//       Math.floor(Math.random() * 256) + ',' +
+//       Math.floor(Math.random() * 256) + ')';
+//     wrapper.style.background = randomColor;
+//   });
+// });
 
 
 
